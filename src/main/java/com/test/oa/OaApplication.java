19545ba0bc8ee2132bf5c19cb68f331eb24e8ffb
Nhan Vo <nhanvohmd1619@gmail.com>
2025-06-07 13:53:00 +0700

    [NhanVo] Init commit

---
+++ src/main/java/com/test/oa/OaApplication.java
@@ -0,0 +1,12 @@
+package com.test.oa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OaApplication {
+
+	public static void main(String[] args) {
+	}
+
+}
